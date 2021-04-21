ef96d4ea2eec5290737cf19ebff12cd002e65c19
eyatsenko <eduardyacenko@gmail.com>
2021-04-21 09:39:50 +0300

    SumOfColumns is completed

--- src/main/java/org/example/homeworks/homework03/SumOfColumns.java
+++ src/main/java/org/example/homeworks/homework03/SumOfColumns.java
@@ -9,5 +9,21 @@ If columns are not of the same size, print which column has bigger size
 */
 
 public class SumOfColumns {
+    public static void main(String[] args) {
+        int[] column1 = {1, 4, 5, 6, 7, 4, 9, 3, 6};
+        int[] column2 = {6, 2, 7, 8, 3, 1, 3, 3, 6};
 
+        if (column1.length == column2.length) {
+            for (int i = 0; i < column1.length; i++) {
+                int first = column1[i];
+                int second = column2[i];
+                int sum = first + second;
+                System.out.println("| " + first + " | " + second + " | " + sum + " |");
+            }
+        } else if (column1.length > column2.length) {
+            System.out.println("Column 1 has bigger size!");
+        } else {
+            System.out.println("Column 2 has bigger size!");
+        }
+    }
 }
