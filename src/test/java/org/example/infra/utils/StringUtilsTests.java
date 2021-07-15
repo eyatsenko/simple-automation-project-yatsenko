ae07e7fd4a4c4341f811c3915a06f1f737e05340
eyatsenko <eduardyacenko@gmail.com>
2021-07-15 23:42:49 +0300

    Added Unit tests.

---
+++ src/test/java/org/example/infra/utils/StringUtilsTests.java
@@ -0,0 +1,79 @@
+package org.example.infra.utils;
+
+import org.example.infrastructure.utils.StringUtils;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.example.infrastructure.utils.StringUtils.Mode.*;
+
+public class StringUtilsTests {
+
+    private String expectedString;
+    private String[] actualStringParts;
+    private String actualString;
+
+    @Before
+    public void setUp() {
+        actualString = "";
+    }
+
+    @Test
+    public void testAlpha() {
+
+        expectedString = StringUtils.randomString(ALPHA, 5);
+        actualStringParts = expectedString.replaceAll("[^a-zA-Z ]", "").split("\\s+");
+
+        for (String s : actualStringParts) {
+            actualString += s;
+        }
+
+        Assert.assertEquals(expectedString, actualString);
+    }
+
+    @Test
+    public void testNumeric () {
+
+        expectedString = StringUtils.randomString(NUMERIC, 5);
+        actualStringParts = expectedString.replaceAll("[^0-9]", "").split("\\s+");
+
+        for (String s : actualStringParts) {
+            actualString += s;
+        }
+
+        Assert.assertEquals(expectedString, actualString);
+    }
+
+    @Test
+    public void testAlphaNumeric () {
+
+        expectedString = StringUtils.randomString(ALPHANUMERIC, 5);
+        actualStringParts = expectedString.replaceAll("[^a-zA-Z0-9]", "").split("\\s+");
+
+        for (String s : actualStringParts) {
+            actualString += s;
+        }
+
+        Assert.assertEquals(expectedString, actualString);
+    }
+
+    @Test
+    public void testHex () {
+
+        expectedString = StringUtils.randomString(HEX, 5);
+        actualStringParts = expectedString.replaceAll("[^A-F0-9]", "").split("\\s+");
+
+        for (String s : actualStringParts) {
+            actualString += s;
+        }
+
+        Assert.assertEquals(expectedString, actualString);
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void testNullMode () {
+
+        StringUtils.randomString(null, 5);
+
+    }
+}
