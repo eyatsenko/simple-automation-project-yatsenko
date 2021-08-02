e417104105b0fbe4bfec26828cc475b4987b4ff5
eyatsenko <eduardyacenko@gmail.com>
2021-08-02 12:52:09 +0300

    Lecture 27

---
+++ src/test/java/org/example/web/wp/Sample.java
@@ -0,0 +1,15 @@
+package org.example.web.wp;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sample {
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+
+        Thread.sleep(3_000);
+
+        driver.close();
+    }
+}
